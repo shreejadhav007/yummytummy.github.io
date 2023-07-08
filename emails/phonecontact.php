149d9b3d5777d34e8a09fede820efb74930bdb5c
shreejadhav007 <shriyashjadhav007@gmail.com>
2023-07-08 21:44:50 +0530

    send

---
+++ emails/phonecontact.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/functions.php';
+require_once __DIR__.'/config.php';
+
+session_start();
+
+// Basic check to make sure the form was submitted.
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    redirectWithError("The form must be submitted with POST data.");
+}
+
+
+// Everything seems OK, time to send the email.
+
+$mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->setLanguage(CONTACTFORM_LANGUAGE);
+    $mail->SMTPDebug = CONTACTFORM_PHPMAILER_DEBUG_LEVEL;
+    $mail->isSMTP();
+    $mail->Host = CONTACTFORM_SMTP_HOSTNAME;
+    $mail->SMTPAuth = true;
+    $mail->Username = CONTACTFORM_SMTP_USERNAME;
+    $mail->Password = CONTACTFORM_SMTP_PASSWORD;
+    $mail->SMTPSecure = CONTACTFORM_SMTP_ENCRYPTION;
+    $mail->Port = CONTACTFORM_SMTP_PORT;
+    $mail->CharSet = CONTACTFORM_MAIL_CHARSET;
+    $mail->Encoding = CONTACTFORM_MAIL_ENCODING;
+
+    // Recipients
+    $mail->setFrom(CONTACTFORM_FROM_ADDRESS, CONTACTFORM_FROM_NAME);
+    $mail->addAddress(CONTACTFORM_TO_ADDRESS, CONTACTFORM_TO_NAME);
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+
+    // Content
+    $mail->Subject = "[Contact me] ".$_POST['subject'];
+    $mail->Body    = <<<EOT
+    Email: {$_POST['email']}
+
+    -------------------------------
+    
+    Message : {$_POST['msg']}
+
+EOT;
+
+    $mail->send();
+    redirectSuccess();
+} catch (Exception $e) {    redirectWithError("An error occurred while trying to send your message: ".$mail->ErrorInfo);
+}
